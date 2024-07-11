6eb20f02c25c2be021f9a4494222e42fdde39f0e
ahaandesai27 <ahaan.desai2705@gmail.com>
2024-07-11 17:35:10 +0530

    Started games backend

---
+++ graphql/resolvers/Mutations/Games.js
@@ -0,0 +1,36 @@
+import Games from '../../../models/games.js'
+
+const urlGenerator = (title) => {
+    //Grand Theft Auto V -> grand-theft-auto-v
+    return title
+        .toLowerCase()
+        .split(" ")
+        .join("-")
+}
+
+const existingCheck = async (title) => {
+    const url = urlGenerator(title)
+    const existing = await Games.findOne({url: url})
+    if (existing) {
+        throw new Error("Game already exists");
+    }
+}
+
+
+const Mutations = {
+    async addGame(_, {game}) {
+        await existingCheck(game.title)
+        return await Games.create(game);
+    },
+
+    async editGame(_, {_id, game}) {
+        await existingCheck(game.title)
+        return await Games.findByIdAndUpdate({_id}, game, {new: true});
+    },
+
+    async deleteGame(_, {_id}) {
+        return await Games.findByIdAndDelete(_id);
+    }
+}
+
+export default Mutations;
---
+++ graphql/resolvers/Mutations/index.js
@@ -0,0 +1,7 @@
+import Games from './Games.js';
+import Users from './Users.js';
+
+export default {
+    ...Games,
+    ...Users
+}
